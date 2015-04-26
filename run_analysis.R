ddee33f4dd2658c65cf446d6f56f9c2e0f09f3f6
Jesús Salceda <jesus.salceda@gmail.com>
2015-04-26 19:14:33 +0200

    Added comments and minor changes
    Added comments at the beginning of the file.
    Eliminated setwd() command

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,14 @@
 ## Filename: run_analysis.R
 ## Author: jesus.salceda@gmail.com
 ## Date: April 2015
-
-## Set working Directory
-setwd("~/Coursera/Getting and Cleaning Data/PA")
+## Comment: Script to prepare tidy data that can be used for later analysis:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for 
+##    each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data 
+##    set with the average of each variable for each activity and each subject.
 
 ## Load libraries
 ## dplyr to use select() function (Step 3)
